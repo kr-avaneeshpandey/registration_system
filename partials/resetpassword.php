60c1426753512b5299a5c49a80aaccf7e51142ba
Avaneesh <avaneesh.pandey2001@gmail.com>
2023-10-28 20:41:11 +0530

    modified icon

--- partials/resetpassword.php
+++ partials/resetpassword.php
@@ -32,7 +32,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="icon" href="partials/icon(1).png">
+    <link rel="icon" href="icon(1).png">
     <title>Reset Password</title>
   </head>
   <body>
